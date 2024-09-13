2c79e93b8e84e44342962a2f852e48669ec58877
Anmol Mishra <anmolmishra2005a@gmail.com>
2024-09-13 18:25:40 +0530

    2

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,9 +13,9 @@ const Navbar = () => (
     className={`${styles.xPaddings} py-8 relative`}
   >
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      <a href="https://jvic-waitlist.vercel.app/"> <img src="/clock.png" alt="search" className="pt-2 w-[44px] h-[44px] object-contain" /> </a>
+      <a href="https://wait.jvic.in/"> <img src="/clock.png" alt="search" className="pt-2 w-[44px] h-[44px] object-contain" /> </a>
       <img src="JVIC.png" className="h-20 w-20" />
-      <a href="https://jvic-book.vercel.app/"> <img src="/book.png" alt="menu" className="w-[50px] h-[50px] object-contain" /> </a>
+      <a href="https://products.jvic.in/"> <img src="/book.png" alt="menu" className="w-[50px] h-[50px] object-contain" /> </a>
     </div>
   </motion.nav>
 );
